0204cafee07d1925eefbc3f122dc2ac5749ceded
Felipe Negri <felinegri5@gmail.com>
2022-06-20 20:13:19 -0300

    Add db project to the sample web api

---
+++ sample/Sample.Web.Database/dbo/Tables/PaymentItems.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PaymentItems] (
+    [Id]        INT              IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (127)   NOT NULL,
+    [Amount]    INT              DEFAULT 1 NOT NULL,
+    [Price]     DECIMAL (15, 2)  NOT NULL,
+    [PaymentId] UNIQUEIDENTIFIER NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_PaymentItems_PaymentId] FOREIGN KEY ([PaymentId]) REFERENCES [dbo].[Payments] ([Id])
+);
+
---
+++ sample/Sample.Web.Database/dbo/Tables/Payments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Payments] (
+    [Id]         UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+    [Currency]   VARCHAR (3)      NOT NULL,
+    [State]      NVARCHAR (MAX)   DEFAULT 'Waiting' NOT NULL,
+    [Provider]   NVARCHAR (MAX)   NOT NULL,
+    [CreateDate] DATETIME         DEFAULT GETUTCDATE() NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
